0ded50c982e476d960b0c906cf0d5c5c4c904649
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-01 13:51:34 +0100

    Added new features.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
+import clipboard
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+?'
 
-number = input("Numbers of password ")
+print("Welcome to the latest sojjyCodes program using python. ")
+number = input("Numbers of password: ")
 number = int(number)
 
-length = input("The length ")
+length = input("The length: ")
 length = int(length)
 
 for p in range(number):
@@ -13,3 +15,4 @@ for p in range(number):
     for c in range(length):
         password += random.choice(chars)
     print(password)
+    text = clipboard.copy(password)
