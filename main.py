c3e5a3c28c24ca2ba396a32f41bea1984309230f
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-15 15:03:17 +0100

    Added some things.

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import clipboard
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+?'
 
-print("Welcome to the latest sojjyCodes program using python. ")
-number = input("Numbers of password: ")
-number = int(number)
+while True:
+    print("Welcome to the latest sojjyCodes program using python. ")
+    number = int(input("Numbers of password: "))
 
-length = input("The length: ")
-length = int(length)
+    length = int(input("The length: "))
 
-for p in range(number):
-    password = ''
-    for c in range(length):
-        password += random.choice(chars)
-    print(password)
-    text = clipboard.copy(password)
-    print("The last password has been added to your clipboard!")
+    for p in range(number):
+        password = ''
+        for c in range(length):
+            password += random.choice(chars)
+        print(password)
+        text = clipboard.copy(password)
+    print("The last password generated has been added to your clipboard!")
+    
+    exit = input("Type exit and click enter to exit\n")
+    if exit == 'exit':
+        print("Thanks for using me :)")
+        break
