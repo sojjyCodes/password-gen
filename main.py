e964e22f055bbcdaf7dc369cd0eee42d9490f35b
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-07-23 13:16:44 +0100

    Added some thing

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import random
 import clipboard
-
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+?'
 
 while True:
     print("Welcome to the latest sojjyCodes program using python. ")
     number = int(input("Numbers of password: "))
-
     length = int(input("The length: "))
 
     for p in range(number):
         password = ''
-        for c in range(length):
-            password += random.choice(chars)
+    for c in range(length):
+        password += random.choice(chars)
         print(password)
         text = clipboard.copy(password)
+
     print("The last password generated has been added to your clipboard!")
+
+    operation = input("Do you want to continue (Y/N): ")
+    if operation == "Y":
+        print("Let get started")
     
-    exit = input("Type exit and click enter to exit\n")
-    if exit == 'exit':
-        print("Thanks for using me :)")
-        break
+    elif operation == "N":
+        break 
+    else:
+        print("Invalid Command")
